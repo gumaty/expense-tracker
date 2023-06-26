97e1dd86777ebe0a3ec681146ce2eabfe8770559
Arkadiusz Matlak <piccolo@poczta.fm>
2023-06-26 09:24:22 +0200

    refactoring

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,12 +1,13 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "../UI/Card";
 
 function Expenses(props) {
 
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {props.expenses.map(expense => (<ExpenseItem key={expense.id} expense={expense}/>))}
-        </div>
+        </Card>
     );
 }
 
